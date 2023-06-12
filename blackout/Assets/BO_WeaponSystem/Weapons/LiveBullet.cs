d79242f65eda7924aee9150661a842fe042669d0
magnet15s <mgns82shmy@gmail.com>
2023-06-12 16:58:02 +0900

    武器関連フォルダ整理＆弾描画途中

--- blackout/Assets/BO_WeaponSystem/Weapons/LiveBullet.cs
+++ blackout/Assets/BO_WeaponSystem/Weapons/LiveBullet.cs
@@ -12,6 +12,11 @@ public class LiveBullet : MonoBehaviour
     public float damage;
     public float generatedTime;
     public int age;
+    public LineRenderer line;
+    public Vector3[] linePosArray;
+    
+    public static bool GET_PREFAB = false;
+    public static GameObject PR_LIVEBULLET;
 
     public static LiveBullet BulletInstantiate(
         Weapon _shooter,
@@ -19,14 +24,16 @@ public class LiveBullet : MonoBehaviour
         Vector3 _initialVelocity, 
         float _damage 
     ){
-        GameObject bullet = new GameObject()
-        {
-            name = "bullet" + _shooter.sender.ToString() + " " + Time.frameCount.ToString()
-        };
-        
+        if (!GET_PREFAB) {
+            PR_LIVEBULLET = ((GameObject)Resources.Load("/Assets/BO_WeaponSystem/Prefabs/PrLiveBullet")) ?? new GameObject { name = "Bullet Load Error" };
+            GET_PREFAB = true;
+        }
+
+        GameObject bullet = Instantiate(PR_LIVEBULLET);
+
+        bullet.name = "bullet" + _shooter.sender.ToString() + " " + Time.frameCount.ToString();
         bullet.transform.position = _initialPosition;
 
-        bullet.AddComponent<LiveBullet>();
         LiveBullet bulletLB = bullet.GetComponent<LiveBullet>();
 
         bulletLB.initialVelocity = _initialVelocity;
@@ -35,13 +42,17 @@ public class LiveBullet : MonoBehaviour
         bulletLB.damage = _damage;
         bulletLB.generatedTime = Time.frameCount;
 
+        LineRenderer bulletLR = bullet.GetComponent<LineRenderer>();
+        bulletLR.positionCount = 1;
+        bulletLR.SetPosition(0, _initialPosition);
         return bulletLB;
     }
 
 
     void Start()
     {
-        
+        line = GetComponent<LineRenderer>();
+        line.GetPositions(linePosArray);
     }
 
     // Update is called once per frame
@@ -50,9 +61,15 @@ public class LiveBullet : MonoBehaviour
         velocity.y -= 9.8f * Time.deltaTime;
         velocity.x -= velocity.x * 0.9f * Time.deltaTime;
         velocity.z -= velocity.z * 0.9f * Time.deltaTime;
-        Vector3 np = transform.position;
-        transform.position = new Vector3(np.x + velocity.x, np.y + velocity.y, np.z + velocity.z);
-        Debug.DrawRay(np, velocity, Color.yellow, 0.1f);
+        Vector3 p = transform.position;
+
+        Vector3 np = p + velocity;
+        transform.position = np;
+        if (new Vector3(Mathf.Abs(linePosArray[0].x - np.x), Mathf.Abs(linePosArray[0].y - np.y), Mathf.Abs(linePosArray[0].z - np.z)).magnitude > 5) {
+            
+        }
+
+
         if(transform.position.y < -10) {
             Destroy(this.gameObject);
         }
