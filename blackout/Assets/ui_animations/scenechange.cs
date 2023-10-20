91dbd808a0b13cb7899290fc4e1ee7793db29139
No11 <144187385+No11-ELEVEN@users.noreply.github.com>
2023-10-20 15:05:15 +0900

    タイトル画面&メインメニュー(仮)

---
+++ blackout/Assets/ui_animations/scenechange.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scenechange : MonoBehaviour
+
+{
+    string Sname;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sname = SceneManager.GetActiveScene().name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnButtonClick() {
+        if (Sname == "menu_01") {
+            Initiate.Fade("menu_02", Color.black, 1.0f);
+        }
+        else {
+            Initiate.Fade("menu_01", Color.black, 1.0f);
+        }
+    }
+}
