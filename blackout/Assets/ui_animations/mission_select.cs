35d413e21cfb81a32f00c96ae37a4e3061027c19
No11 <144187385+No11-ELEVEN@users.noreply.github.com>
2023-11-13 13:19:31 +0900

    軽量フォント追加

--- blackout/Assets/ui_animations/mission_select.cs
+++ blackout/Assets/ui_animations/mission_select.cs
@@ -1,30 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class mission_select : MonoBehaviour
 {
+    [SerializeField] private GameObject _loadingUI;
+    [SerializeField] private Slider _slider;
     public static int mission_number=0;
     // Start is called before the first frame update
     public void OnButtonClick01() {
         mission_number = 1;
-        Initiate.Fade("menu_04", Color.black, 3f);
+        _loadingUI.SetActive(true);
+        StartCoroutine(LoadScene());
     }
     public void OnButtonClick02() {
         mission_number = 2;
-        Initiate.Fade("menu_04", Color.black, 3f);
+        _loadingUI.SetActive(true);
+        StartCoroutine(LoadScene());
     }
     public void OnButtonClick03() {
         mission_number = 3;
-        Initiate.Fade("menu_04", Color.black, 3f);
+        _loadingUI.SetActive(true);
+        StartCoroutine(LoadScene());
     }
     public void OnButtonClick04() {
         mission_number = 4;
-        Initiate.Fade("menu_04", Color.black, 3f);
+        _loadingUI.SetActive(true);
+        StartCoroutine(LoadScene());
     }
     public void OnButtonClick05() {
         mission_number = 5;
-        Initiate.Fade("menu_04", Color.black, 3f);
+        _loadingUI.SetActive(true);
+        StartCoroutine(LoadScene());
     }
     public void OnButtonClickRe() {
         Initiate.Fade("menu_02", Color.black, 3f);
@@ -32,5 +41,14 @@ public class mission_select : MonoBehaviour
 
     public void OnButtonClickInmenu() {
         Initiate.Fade("menu_03", Color.black, 3f);
+    }
+
+    IEnumerator LoadScene() {
+        AsyncOperation async = SceneManager.LoadSceneAsync("menu_04");
+
+        while (!async.isDone) {
+            _slider.value = async.progress;
+            yield return null;
+        }
     }
 }
