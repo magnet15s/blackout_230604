51b988d1dec7f1724d96962f0869276dade59ac5
No11 <144187385+No11-ELEVEN@users.noreply.github.com>
2023-12-12 14:58:17 +0900

    メニューからミッション本編へ移る際のロード画面テスト

--- blackout/Assets/ui_animations/text_load.cs
+++ blackout/Assets/ui_animations/text_load.cs
@@ -1,6 +1,8 @@
 using TMPro;
 using UnityEngine;
+using System.Collections;
 using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class text_load : MonoBehaviour {
@@ -8,6 +10,8 @@ public class text_load : MonoBehaviour {
     public TextMeshProUGUI text;
     public Image image;
     public Sprite sprite;
+    [SerializeField] private GameObject _loadingUI;
+    [SerializeField] private Slider _slider;
 
     void Start() {
         // テキストファイルの全文を取得
@@ -40,19 +44,70 @@ public class text_load : MonoBehaviour {
 
     public void OnButtonClickSyuutugeki() {
         if (mission_select.mission_number == 1) {
-            Initiate.Fade("world01", Color.black, 3f);
+            //Initiate.Fade("world01", Color.black, 3f);
+            _loadingUI.SetActive(true);
+            StartCoroutine(LoadScene1());
         }
         else if (mission_select.mission_number == 2) {
-            Initiate.Fade("world02", Color.black, 3f);
+            //Initiate.Fade("world02", Color.black, 3f);
+            _loadingUI.SetActive(true);
+            StartCoroutine(LoadScene2());
         }
         else if (mission_select.mission_number == 3) {
-            Initiate.Fade("world03", Color.black, 3f);
+            //Initiate.Fade("world03", Color.black, 3f);
+            _loadingUI.SetActive(true);
+            StartCoroutine(LoadScene3());
         }
         else if (mission_select.mission_number == 4) {
-            Initiate.Fade("world04", Color.black, 3f);
+            //Initiate.Fade("world04", Color.black, 3f);
+            _loadingUI.SetActive(true);
+            StartCoroutine(LoadScene4());
         }
         else if (mission_select.mission_number == 5) {
-            Initiate.Fade("world05", Color.black, 3f);
+            //Initiate.Fade("world05", Color.black, 3f);
+            _loadingUI.SetActive(true);
+            StartCoroutine(LoadScene5());
+        }
+    }
+
+    IEnumerator LoadScene1() {
+        AsyncOperation async = SceneManager.LoadSceneAsync("world01");
+
+        while (!async.isDone) {
+            _slider.value = async.progress;
+            yield return null;
+        }
+    }
+    IEnumerator LoadScene2() {
+        AsyncOperation async = SceneManager.LoadSceneAsync("world02");
+
+        while (!async.isDone) {
+            _slider.value = async.progress;
+            yield return null;
+        }
+    }
+    IEnumerator LoadScene3() {
+        AsyncOperation async = SceneManager.LoadSceneAsync("world03");
+
+        while (!async.isDone) {
+            _slider.value = async.progress;
+            yield return null;
+        }
+    }
+    IEnumerator LoadScene4() {
+        AsyncOperation async = SceneManager.LoadSceneAsync("world04");
+
+        while (!async.isDone) {
+            _slider.value = async.progress;
+            yield return null;
+        }
+    }
+    IEnumerator LoadScene5() {
+        AsyncOperation async = SceneManager.LoadSceneAsync("world05");
+
+        while (!async.isDone) {
+            _slider.value = async.progress;
+            yield return null;
         }
     }
 }
