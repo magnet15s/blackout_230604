10465a15d48ef2dacee37d647824d6349ec6e986
No11 <144187385+No11-ELEVEN@users.noreply.github.com>
2024-02-01 11:07:55 +0900

    ミッション02_完成

---
+++ blackout/Assets/Scripts/Mission/Mission_02/M02_t2.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class M02_t2 : MissionEventNode 
+    {
+    // Start is called before the first frame update
+    public override void EventFire() {
+        StartCoroutine("Event");
+    }
+    private string[] messageText = new string[]{
+        "[キース]\n掃討完了。",
+        "[オスカー大佐]\nご苦労。ドローンの投入を開始する。",
+        "[オスカー大佐]\nミッション完了だ。"
+    };
+    [SerializeField] MessageWindow message;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator Event() {
+        yield return new WaitForSeconds(1);
+        message.function(messageText[0], 2.7f);
+        yield return new WaitForSeconds(5);
+        message.function(messageText[1], 4.7f);
+        yield return new WaitForSeconds(5);
+        message.function(messageText[2], 3.7f);
+        ParmitNext();
+
+    }
+}
