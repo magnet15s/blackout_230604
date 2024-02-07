06505620de019303e5ca4b433de76bf8817dce8e
magnet15s <mgns82shmy@gmail.com>
2024-02-07 13:56:53 +0900

    kari

--- blackout/Assets/Scripts/Enemy/EnemyCore_Components/Mission3Stay.cs
+++ blackout/Assets/Scripts/Enemy/EnemyCore_Components/Mission3Stay.cs
@@ -6,17 +6,36 @@ using UnityEngine.AI;
 public class Mission3Stay : MonoBehaviour {
     [SerializeField] EnemyCore core;
     [SerializeField] NavMeshAgent agent;
-    //[SerializeField] float secPerTan = 0.1f; 
+    [SerializeField] Transform center;
+    [SerializeField] float radius;
+    [SerializeField] float radDamp;
     private bool notTargetContact = true;
     private delegate void M3sm();
     private M3sm m3sm;
+    private void Awake() {
+        m3sm += MoveField;
+    }
     public void Mission3StayMove() {
         m3sm?.Invoke();
     }
 
     private void MoveField() {
         if (agent.pathStatus != NavMeshPathStatus.PathInvalid) {
+            Vector3 targetForward;
+            float outin;
 
+            if (Mathf.Abs(outin = ((center.position - transform.position).magnitude - radius)) > radDamp) {
+                //半径修正
+                targetForward = center.position - transform.position * Mathf.Sign(outin);
+                targetForward.Normalize();
+            } else {
+                //巡回
+                //upベクトルと自ワールド座標 - 中央座標ベクトルの外積方向へ
+                Debug.Log("ddd");
+                targetForward = Vector3.Cross(Vector3.up, (transform.position - center.position).normalized);
+            }
+
+            agent.destination = targetForward;
         }
     }
 
@@ -24,7 +43,7 @@ public class Mission3Stay : MonoBehaviour {
     private void Update() {
         if (core.targetFound && notTargetContact) {
             notTargetContact = false;
-            //core.
+            m3sm -= MoveField;
         }
     }
 }
