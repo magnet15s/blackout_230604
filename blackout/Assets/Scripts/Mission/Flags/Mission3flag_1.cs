f064dcc2c66b5dcfb90e75508a0669c78e7180ec
No11 <144187385+No11-ELEVEN@users.noreply.github.com>
2024-02-08 15:53:14 +0900

    ミッション03_実装完了

---
+++ blackout/Assets/Scripts/Mission/Flags/Mission3flag_1.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Mission3flag_1 : MissionEventFlag {
+    // Start is called before the first frame update
+    public GameObject player;
+    private float kyori;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        kyori = (this.gameObject.transform.position - player.transform.position).magnitude;
+        if(kyori <= 300) {
+            OnFlagUp();
+        }
+    }
+    
+    /*private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            OnFlagUp();
+        }
+    }*/
+}
